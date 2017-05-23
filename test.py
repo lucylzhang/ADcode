f6442e18de811d9a2dbe279fd758fb6f328c737f
lucylzhang <luluzhangIT@outlook.com>
2017-05-23 10:34:22 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -7,39 +7,66 @@ import xgboost as xgb
 import pandas as pd
 import numpy as np
 import time
-
+import matplotlib.pyplot as plt  
+import operator
 # In[3]:
 PATH_ROOT = r'G:\python\data'
 PATH_DATA_TRAIN_ALL = PATH_ROOT + r'\data_train_all.csv'
 PATH_DATA_TEST = PATH_ROOT + r'\data_test_all.csv'
 TIME_TRAIN = time.strftime('%m-%d-%H-%M',time.localtime(time.time()))
+PATH_SUBMIT_MODEL = PATH_ROOT + '\\' + TIME_TRAIN + r'.raw.txt'
 PATH_SUBMIT = PATH_ROOT + '\\' + TIME_TRAIN + r'.csv'
+
+print(PATH_SUBMIT)
 
 # In[4]:
 
 # -----------------导入数据，dtrain表示训练数据，dval表示验证数据
 # -----------------train_label表示训练标签，val_label表示测试标签
 data_train_all = pd.read_csv(PATH_DATA_TRAIN_ALL)
-#数据预处理-训练数据
-data_train_day = [17,18,19,20,21,22, 24,25,26,27,28,29] 
 
+#------------------数据预处理-训练数据
+data_train_day = [23,24,25,26,27,28,29] 
 data_train = data_train_all[ (data_train_all['clickTime'] // 10000).isin(data_train_day)]
-data_train['clickTime'] = data_train['clickTime'] // 100 % 100 * 60  + data_train['clickTime'] % 100
 
-#数据预处理-验证数据
-data_val_day = [23,30] 
+#分解点击时间： 小时 分钟
+data_train['clickTimeHour'] = data_train['clickTime'] // 100 % 100
+data_train['clickTimeMinute'] = data_train['clickTime'] % 100
+
+#分解家乡： 省份 城市
+data_train['hometown_province'] = data_train['hometown'] // 100 % 100
+data_train['hometown_city'] = data_train['hometown'] % 100
+
+#分解现居地： 省份 城市
+data_train['residence_province'] = data_train['residence'] // 100 % 100
+data_train['residence_city'] = data_train['residence'] % 100
+
+
+#------------------数据预处理-验证数据
+data_val_day = [30] 
 data_val = data_train_all[ (data_train_all['clickTime'] // 10000).isin(data_val_day)]
-data_val['clickTime'] = data_val['clickTime'] // 100 % 100 * 60  + data_val['clickTime'] % 100
+
+#分解点击时间： 小时 分钟
+data_val['clickTimeHour'] = data_val['clickTime'] // 100 % 100
+data_val['clickTimeMinute'] = data_val['clickTime'] % 100
+
+#分解家乡： 省份 城市
+data_val['hometown_province'] = data_val['hometown'] // 100 % 100
+data_val['hometown_city'] = data_val['hometown'] % 100
+
+#分解现居地： 省份 城市
+data_val['residence_province'] = data_val['residence'] // 100 % 100
+data_val['residence_city'] = data_val['residence'] % 100
+
 
 #指定训练数据、验证数据 及 相应的标签
 train_label = data_train['label'].values
-data_train.drop(['instanceID', 'label', 'conversionTime'], axis=1, inplace=True)
+data_train.drop(['instanceID', 'label', 'conversionTime', 'clickTime'], axis=1, inplace=True)
 train_data = data_train.values
 
 val_label = data_val['label'].values
-data_val.drop(['instanceID', 'label', 'conversionTime'], axis=1, inplace=True)
+data_val.drop(['instanceID', 'label', 'conversionTime',  'clickTime' ], axis=1, inplace=True)
 val_data = data_val.values
-
 
 
 
@@ -81,11 +108,24 @@ num_round =100000
 print(plst)
 bst = xgb.train(plst, xgb_dtrain, num_round, evallist, early_stopping_rounds=10)
 
+# In[66]:
+
+#特征选择
+importance = bst.get_fscore()  
+importance = sorted(importance.items(), key=operator.itemgetter(1))  
+df = pd.DataFrame(importance, columns=['feature', 'fscore'])
+
+df['feature'].map()
+plt.figure()  
+df.plot(kind='barh', x='feature', y='fscore', legend=False, figsize=(6, 10))  
+plt.title('XGBoost Feature Importance')  
+plt.xlabel('relative importance')  
+plt.show() 
 
 # In[66]:
 
 # ------保存模型-----------------
-bst.dump_model('dump05211755.raw.txt')
+bst.dump_model(PATH_SUBMIT_MODEL)
 # bst.dump_model('dump.raw.txt', 'featmap.txt')
 
 
@@ -93,8 +133,20 @@ bst.dump_model('dump05211755.raw.txt')
 
 # --------------读入数据并调用模型进行预测-----------------
 data_test = pd.read_csv(PATH_DATA_TEST)
-data_test['clickTime'] = data_test['clickTime'] // 100 % 100 * 60  + data_test['clickTime'] % 100
-data_test_ = data_test.drop(['instanceID', 'label'], axis=1)
+#分解点击时间
+data_test['clickTimeHour'] = data_test['clickTime'] // 100 % 100
+data_test['clickTimeMinute'] = data_test['clickTime'] % 100
+
+#分解家乡
+data_test['hometown_province'] = data_test['hometown'] // 100 % 100
+data_test['hometown_city'] = data_test['hometown'] % 100
+
+#分解现居地： 省份 城市
+data_test['residence_province'] = data_test['residence'] // 100 % 100
+data_test['residence_city'] = data_test['residence'] % 100
+
+
+data_test_ = data_test.drop(['instanceID', 'label', 'clickTime'], axis=1)
 
 xgb_dtest = xgb.DMatrix(data_test_.values, missing=0.0)
 ypred = bst.predict(xgb_dtest, ntree_limit=bst.best_iteration)
